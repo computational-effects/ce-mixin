251d4668f47d9378e5690622fa03827b472af0ed
joneshf <jones3.hardy@gmail.com>
2015-08-04 07:28:23 -0700

    Extract mixin stuff to its own package

---
+++ src/index.js
@@ -0,0 +1,19 @@
+'ues strict';
+
+var mixin = function(mix, obj) {
+  Object.keys(mix).forEach(function(key) {
+    obj[key] = mix[key];
+  });
+  return obj;
+};
+
+var mixins = function(mixs, obj) {
+  return mixs.reduce(function(obj, mix) {
+    return mixin(mix, obj);
+  }, obj);
+};
+
+module.exports = {
+  mixin: mixin
+  mixins: mixins
+};
